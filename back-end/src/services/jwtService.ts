94f16006d181c535e171477f8b9495c27f3f3112
luiz2k <luizteles934@gmail.com>
2024-07-10 10:32:22 -0300

    Service responsável por lidar com o json web token

---
+++ back-end/src/services/jwtService.ts
@@ -0,0 +1,93 @@
+import jwt from "jsonwebtoken";
+import { UnauthorizedError } from "../helpers/errorHandler";
+import { env } from "../validations/envValidation";
+
+import type { IJwtRepository } from "../repositories/interfaces/IJwtRepository";
+import type { GenerateTokenOutput, Payload } from "../types/jwt";
+import type { IJwtService } from "./interfaces/IJwtService";
+
+export class JwtService implements IJwtService {
+	constructor(private readonly jwtRepository: IJwtRepository) {}
+
+	public async verifyToken(token: string): Promise<Payload | false> {
+		try {
+			const decoded = jwt.verify(token, env.REFRESH_TOKEN_SECRET) as Payload;
+
+			return decoded;
+		} catch {
+			return false;
+		}
+	}
+
+	public async generateTokens(userId: string): Promise<GenerateTokenOutput> {
+		const TIMESTAMP_IN_MILLISECONDS = Date.now();
+		const TIMESTAMP_IN_SECONDS = Math.floor(TIMESTAMP_IN_MILLISECONDS / 1000);
+
+		const EXPIRES_IN_ONE_HOUR = TIMESTAMP_IN_SECONDS + 60 * 60;
+		const EXPIRES_IN_SEVEN_DAYS = TIMESTAMP_IN_SECONDS + 60 * 60 * 24 * 7;
+
+		const accessTokenPayload: Payload = {
+			id: userId,
+			iat: TIMESTAMP_IN_SECONDS,
+			exp: EXPIRES_IN_ONE_HOUR,
+		};
+
+		const refreshTokenPayload: Payload = {
+			id: userId,
+			iat: TIMESTAMP_IN_SECONDS,
+			exp: EXPIRES_IN_SEVEN_DAYS,
+		};
+
+		const accessToken = jwt.sign(accessTokenPayload, env.ACCESS_TOKEN_SECRET);
+		const refreshToken = jwt.sign(
+			refreshTokenPayload,
+			env.REFRESH_TOKEN_SECRET,
+		);
+
+		await this.jwtRepository.createRefreshToken({
+			userId: userId,
+			token: refreshToken,
+			createdAt: new Date(TIMESTAMP_IN_MILLISECONDS),
+			expiresAt: new Date(EXPIRES_IN_SEVEN_DAYS * 1000),
+		});
+
+		return {
+			access: {
+				token: accessToken,
+				expiresIn: TIMESTAMP_IN_MILLISECONDS,
+			},
+			refresh: {
+				token: refreshToken,
+			},
+		};
+	}
+
+	public async refreshToken(
+		userId: string,
+		token: string,
+	): Promise<GenerateTokenOutput> {
+		const refreshToken = await this.jwtRepository.findRefreshToken({
+			userId: userId,
+			token: token,
+		});
+
+		if (!refreshToken) {
+			throw new UnauthorizedError("Token inválido.");
+		}
+
+		await this.jwtRepository.removeRefreshToken({
+			userId: userId,
+			token: token,
+		});
+
+		await this.jwtRepository.createRevokedToken({
+			userId: String(refreshToken.userId),
+			token: refreshToken.token,
+			revokedAt: new Date(),
+		});
+
+		const newTokens = await this.generateTokens(userId);
+
+		return newTokens;
+	}
+}

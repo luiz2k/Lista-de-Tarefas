8f81767cc40dddbe0f515a6238f1929e820fd156
luiz2k <luizteles934@gmail.com>
2024-07-10 10:56:52 -0300

    Middleware para lidar com os erros

---
+++ back-end/src/middlewares/errorMiddleware.ts
@@ -0,0 +1,28 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+import type { NextFunction, Request, Response } from "express";
+import type { GenericError } from "../helpers/errorHandler.js";
+
+class ErrorMiddleware {
+	public handle(
+		error: Error & GenericError,
+		_req: Request,
+		res: Response,
+		_next: NextFunction,
+	) {
+		const statusCode: number = error.statusCode ?? 500;
+		const zodErrors = error.zodErrors;
+
+		const message: string = error.statusCode
+			? error.message
+			: "Erro interno do servidor.";
+
+		return res.status(error.statusCode).json({
+			statusCode: statusCode,
+			message: message,
+			paths: zodErrors,
+		});
+	}
+}
+
+export const errorMiddleware = new ErrorMiddleware();

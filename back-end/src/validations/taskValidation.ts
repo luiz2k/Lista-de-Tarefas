3d414ee22c884656a26ee8dbcebcdefe29272b4e
luiz2k <luizteles934@gmail.com>
2024-07-14 15:52:42 -0300

    Alterar a validação do esquema de criar uma tarefa

--- back-end/src/validations/taskValidation.ts
+++ back-end/src/validations/taskValidation.ts
@@ -3,14 +3,14 @@ import z from "zod";
 export const createTaskSchema = z.object({
 	task: z
 		.string()
-		.min(1, "Por favor, digite uma tarefa.")
+		.min(2, "A tarefa deve ter pelo menos 2 caracteres.")
 		.max(20, "A tarefa deve ter no máximo 20 caracteres."),
 });
 
 export const updateTaskSchema = z.object({
 	task: z
 		.string()
-		.min(1, "Por favor, digite uma tarefa.")
+		.min(2, "A tarefa deve ter pelo menos 2 caracteres.")
 		.max(20, "A tarefa deve ter no máximo 20 caracteres.")
 		.optional(),
 	completed: z.boolean().optional(),

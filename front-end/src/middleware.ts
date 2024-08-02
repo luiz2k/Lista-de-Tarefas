442905a502689f089bdf6deb85481ce68447dc37
luiz2k <luizteles934@gmail.com>
2024-08-02 09:52:13 -0300

    Melhorar e desacoplar middleware responsável pelas rotas e refresh token

--- front-end/src/middleware.ts
+++ front-end/src/middleware.ts
@@ -1,76 +1,52 @@
 import { NextResponse } from "next/server";
-import { API } from "./modules/shared/utils/ApiConnection";
+import { getSession } from "./modules/shared/helpers/getSession";
+import { refreshToken } from "./modules/shared/services/refreshToken";
 
 import type { NextRequest } from "next/server";
-import type { Tokens } from "./modules/auth/pages/SignIn/types";
-
-const handleRefreshToken = async (refreshToken: string) => {
-	try {
-		const response = await API.request<Tokens>({
-			method: "POST",
-			path: "/auth/refreshToken",
-			body: JSON.stringify({
-				refreshToken: refreshToken,
-			}),
-			cache: "no-store",
-		});
-
-		if (response.statusCode === 400 && response.message === "Token inválido.") {
-			throw new Error();
-		}
-
-		const res = NextResponse.next();
-
-		res.cookies.set("session", JSON.stringify(response.data), {
-			httpOnly: true,
-		});
-
-		return res;
-	} catch {
-		const res = NextResponse.next();
-
-		res.cookies.delete("session");
-
-		return res;
-	}
-};
 
 export default async function middleware(req: NextRequest) {
-	const tokens = req.cookies.get("session")?.value || "";
+	try {
+		const session = getSession();
 
-	const authPage =
-		req.nextUrl.pathname === "/registro" || req.nextUrl.pathname === "/entrar";
+		const pathname = req.nextUrl.pathname;
 
-	if (!tokens) {
-		if (authPage) {
-			return NextResponse.next();
+		const authPage = pathname === "/registro" || pathname === "/entrar";
+
+		if (!session) {
+			if (authPage) {
+				return NextResponse.next();
+			}
+
+			return NextResponse.redirect(new URL("/entrar", req.url));
 		}
 
-		return NextResponse.redirect(new URL("/entrar", req.url));
-	}
-
-	if (authPage) {
-		return NextResponse.redirect(new URL("/", req.url));
-	}
-
-	try {
-		const session: Tokens = JSON.parse(tokens);
+		if (authPage) {
+			return NextResponse.redirect(new URL("/", req.url));
+		}
 
 		const expiresIn = session.access.expiresIn;
-		const ONE_MINUTE = 1000 * 60;
+		const TWO_MINUTES = 2000 * 60;
 		const currentDate = Date.now();
 
-		const tokenExpired = currentDate >= expiresIn - ONE_MINUTE;
+		const tokenExpired = currentDate >= expiresIn - TWO_MINUTES;
 
 		if (tokenExpired) {
-			return await handleRefreshToken(session.refresh.token);
+			const response = await refreshToken(session.refresh.token);
+
+			const middResponse = NextResponse.next();
+
+			middResponse.cookies.set("session", JSON.stringify(response.data), {
+				httpOnly: true,
+			});
+
+			return middResponse;
 		}
 	} catch {
-		const res = NextResponse.redirect(new URL("/entrar", req.url));
+		const middResponse = NextResponse.next();
 
-		res.cookies.delete("session");
+		middResponse.cookies.delete("session");
 
-		return res;
+		return middResponse;
 	}
 }
 

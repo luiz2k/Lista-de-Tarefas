3927c50405ec5c3ac3a42220d2a21362e4b64173
luiz2k <luizteles934@gmail.com>
2024-08-02 10:02:44 -0300

    Implementar página de login

---
+++ front-end/src/app/(auth)/entrar/page.tsx
@@ -0,0 +1,10 @@
+import { SignInPage } from "@/modules/auth/pages/SignIn";
+
+import type{ Metadata } from "next";
+
+export const metadata: Metadata = {
+	title: "Lista de Tarefas | Entrar",
+	description: "",
+};
+
+export default SignInPage;

f0091df1fccf266db03b383502ab23a92bd845ad
luiz2k <luizteles934@gmail.com>
2024-08-02 10:03:04 -0300

    Implementar página home

---
+++ front-end/src/app/(dashboard)/page.tsx
@@ -0,0 +1,10 @@
+import { HomePage } from "@/modules/dashboard/pages/Home";
+
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+	title: "Lista de Tarefas | Painel",
+	description: "",
+};
+
+export default HomePage;

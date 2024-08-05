f35195573ab60f7559b02ce4f38e4c469fc6f465
luiz2k <luizteles934@gmail.com>
2024-08-05 19:47:03 -0300

    Implementar teste na busca de tarefas

---
+++ front-end/src/modules/dashboard/pages/Home/components/Header/components/SearchInput/index.spec.tsx
@@ -0,0 +1,87 @@
+import { act, fireEvent, render, screen } from "@testing-library/react";
+import SearchInput from ".";
+import { HomeContextProvider } from "../../../../context";
+import { TaskTable } from "../../../Table";
+
+import type { Task } from "../../../../types";
+
+const tasks: Task[] = [
+	{
+		_id: "task1",
+		userId: "user1",
+		task: "Tarefa 1",
+		completed: false,
+		createdAt: "2023-01-01T00:00:00.000Z",
+	},
+	{
+		_id: "task2",
+		userId: "user2",
+		task: "Tarefa 2",
+		completed: true,
+		createdAt: "2023-01-02T00:00:00.000Z",
+	},
+	{
+		_id: "task3",
+		userId: "user1",
+		task: "Tarefa 3",
+		completed: false,
+		createdAt: "2023-01-03T00:00:00.000Z",
+	},
+];
+
+describe("SearchInput", () => {
+	it("Deve buscar uma tarefa", async () => {
+		render(
+			<HomeContextProvider tasks={tasks}>
+				<SearchInput />
+				<TaskTable />
+			</HomeContextProvider>,
+		);
+
+		const searchInput = screen.getByPlaceholderText(
+			"Busque tarefas...",
+		) as HTMLInputElement;
+
+		fireEvent.change(searchInput, { target: { value: "Tarefa 1" } });
+
+		await act(async () => {
+			await new Promise((resolve) => setTimeout(resolve, 500));
+		});
+
+		expect(searchInput.value).toBe("Tarefa 1");
+
+		expect(screen.getByText("Tarefa 1")).toBeInTheDocument();
+
+		expect(screen.queryByText("Tarefa 2")).not.toBeInTheDocument();
+		expect(screen.queryByText("Tarefa 3")).not.toBeInTheDocument();
+	});
+
+	it("Deve encontrar nenhuma tarefa", async () => {
+		render(
+			<HomeContextProvider tasks={tasks}>
+				<SearchInput />
+				<TaskTable />
+			</HomeContextProvider>,
+		);
+
+		const searchInput = screen.getByPlaceholderText(
+			"Busque tarefas...",
+		) as HTMLInputElement;
+
+		fireEvent.change(searchInput, { target: { value: "Tarefa inexistente" } });
+
+		await act(async () => {
+			await new Promise((resolve) => setTimeout(resolve, 500));
+		});
+
+		expect(searchInput.value).toBe("Tarefa inexistente");
+
+		expect(
+			screen.getByText("Nenhuma tarefa foi encontrada."),
+		).toBeInTheDocument();
+
+		expect(screen.queryByText("Tarefa 1")).not.toBeInTheDocument();
+		expect(screen.queryByText("Tarefa 2")).not.toBeInTheDocument();
+		expect(screen.queryByText("Tarefa 3")).not.toBeInTheDocument();
+	});
+});

c1fd95c74471bc79e9fdcf32590f035de3eeedca
luiz2k <luizteles934@gmail.com>
2024-08-20 13:18:49 -0300

    Configurar o data-source do TypeORM

---
+++ back-end/src/database/data-source.ts
@@ -0,0 +1,17 @@
+import "dotenv/config";
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import { env } from "../validations/envValidation";
+
+export const AppDataSource = new DataSource({
+	type: "postgres",
+	host: env.DB_HOST,
+	port: env.DB_PORT,
+	username: env.DB_USERNAME,
+	password: env.DB_PASSWORD,
+	database: "todo",
+	synchronize: false,
+	logging: false,
+	entities: ["./src/database/entities/*{.ts,.js}"],
+	migrations: ["./src/database/migrations/*{.ts,.js}"],
+});

661695a1a6f004d87975fb0ee91e7fd93372c182
luiz2k <luizteles934@gmail.com>
2024-08-20 13:19:34 -0300

    Validar variáveis de ambiente do banco de dados

--- back-end/src/validations/envValidation.ts
+++ back-end/src/validations/envValidation.ts
@@ -6,6 +6,10 @@ const envSchema = z.object({
 	ACCESS_TOKEN_SECRET: z.string(),
 	REFRESH_TOKEN_SECRET: z.string(),
 	MONGODB_URI: z.string().url(),
+	DB_HOST: z.string(),
+	DB_PORT: z.string().transform((port) => Number(port)),
+	DB_USERNAME: z.string(),
+	DB_PASSWORD: z.string(),
 });
 
 export const env = envSchema.parse(process.env);

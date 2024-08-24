de01c0d955bcea2bcd3fd9324f83a1327fdfcbf2
luiz2k <luizteles934@gmail.com>
2024-08-24 12:30:49 -0300

    Resolver problema de importação do HttpFactory

--- front-end/src/modules/shared/utils/apiConnection/index.ts
+++ front-end/src/modules/shared/utils/apiConnection/index.ts
@@ -1,4 +1,4 @@
-import { HttpFactory } from "@/modules/shared/services/httpClientAdapter";
+import { HttpFactory } from "@/modules/shared/services/httpClientAdapter/index";
 import { env } from "../../validations/envValidation";
 
 export const API = HttpFactory(env.API_BASE_URL);

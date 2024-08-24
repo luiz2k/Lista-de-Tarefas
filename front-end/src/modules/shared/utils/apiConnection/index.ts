0adc418ebb737822d7e8b46e6feb8f2c6104d8b3
luiz2k <luizteles934@gmail.com>
2024-08-24 12:27:49 -0300

    Colocar @ na importação do httpClientAdapter

--- front-end/src/modules/shared/utils/apiConnection/index.ts
+++ front-end/src/modules/shared/utils/apiConnection/index.ts
@@ -1,4 +1,4 @@
-import { HttpFactory } from "../../services/httpClientAdapter";
+import { HttpFactory } from "@/modules/shared/services/httpClientAdapter";
 import { env } from "../../validations/envValidation";
 
 export const API = HttpFactory(env.API_BASE_URL);

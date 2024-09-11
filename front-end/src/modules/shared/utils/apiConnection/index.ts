a7957e8b01032143f6c58c13eec85035d682e317
luiz2k <luizteles934@gmail.com>
2024-09-11 11:17:37 -0300

    Alterar o nome da validação da variável de ambiente

--- front-end/src/modules/shared/utils/apiConnection/index.ts
+++ front-end/src/modules/shared/utils/apiConnection/index.ts
@@ -2,4 +2,4 @@ import { httpFactory } from "../../services/httpClientAdapter";
 import { env } from "../../validations/envValidation";
 
 // Conexão com a API
-export const API = httpFactory(env.API_BASE_URL);
+export const API = httpFactory(env.API_URL);
--- front-end/src/modules/shared/validations/envValidation.ts
+++ front-end/src/modules/shared/validations/envValidation.ts
@@ -1,7 +1,8 @@
 import { z } from "zod";
 
+// Validação das variáveis de ambiente
 const envSchema = z.object({
-	API_BASE_URL: z.string().url(),
+	API_URL: z.string().url(),
 });
 
 export const env = envSchema.parse(process.env);
